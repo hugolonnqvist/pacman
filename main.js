30be01012d3008bf7bf4ebe2ec9a635d365f85e2
Hugo <hugolonnqvistosterberg@gmail.com>
2021-04-23 23:26:20 +0200

    Testing how canvas works

--- main.js
+++ main.js
@@ -1,20 +1,62 @@
-"use strict";
-
-function generateBoard() {
-  const board = document.getElementById("boardContainer");
-  for (let i = 0; i < 200; i++) {
-    const square = document.createElement("div");
-    square.classList.add("square");
-    board.append(square);
-  }
-}
-
-generateBoard();
-
-//Canvas testing
-
+//"use strict";
 const canvas = document.getElementById("canvas");
 const ctx = canvas.getContext("2d");
+let raf;
 
-//ctx.fillStyle = "blue";
-//ctx.fillRect(10, 10, 100, 100);
+let pacman = {
+  xCord: 50,
+  yCord: 50,
+  radius: 20,
+  color: "yellow",
+  drawPacman: function () {
+    ctx.beginPath();
+    ctx.arc(this.xCord, this.yCord, this.radius, 0, Math.PI * 2, true);
+    ctx.closePath();
+    ctx.fillStyle = this.color;
+    ctx.fill();
+  },
+};
+
+function draw() {
+  ctx.clearRect(0, 0, 1000, 800);
+  pacman.drawPacman();
+  pacman.xCord += 2;
+  pacman.yCord += 2;
+  raf = window.requestAnimationFrame(draw);
+}
+
+window.addEventListener("keydown", (e) => {
+  switch (e.key) {
+    case "ArrowUp":
+      raf = window.requestAnimationFrame(draw);
+      break;
+    case "ArrowDown":
+      raf = window.requestAnimationFrame(draw);
+      break;
+    case "ArrowLeft":
+      raf = window.requestAnimationFrame(draw);
+      break;
+    case "ArrowRight":
+      raf = window.requestAnimationFrame(draw);
+      break;
+  }
+});
+
+window.addEventListener("keyup", (e) => {
+  switch (e.key) {
+    case "ArrowUp":
+      window.cancelAnimationFrame(raf);
+      break;
+    case "ArrowDown":
+      window.cancelAnimationFrame(raf);
+      break;
+    case "ArrowLeft":
+      window.cancelAnimationFrame(raf);
+      break;
+    case "ArrowRight":
+      window.cancelAnimationFrame(raf);
+      break;
+  }
+});
+
+pacman.drawPacman();
